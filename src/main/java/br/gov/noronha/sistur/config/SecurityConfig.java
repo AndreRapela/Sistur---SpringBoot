c9be89e567daa7e0d0b92cace4711c16003c995f
André Rapela <andre.d.q.rapela@gmail.com>
2026-03-18 17:02:21 -0300

    mudando cors

--- src/main/java/br/gov/noronha/sistur/config/SecurityConfig.java
+++ src/main/java/br/gov/noronha/sistur/config/SecurityConfig.java
@@ -42,7 +42,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200",
+            "https://sistur-angular.vercel.app",
+            "https://sistur-angular-git-main-andrerapelas-projects.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
